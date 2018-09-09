5d0fd96cb550c5d28b0164c4de85c191306b994f
NancyOrellanaT <adrianaorellana34@gmail.com>
2018-09-09 11:58:15 -0400

    Cambios en el eje de coordenadas

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-camera.position.z = 5;
+camera.position.x = 2;
+camera.position.y = 2;
+camera.position.z = 7;
 
-dibujarLineas(1);
-dibujarLineas(2);
-dibujarLineas(3);
+eje();
 
 var animate = function() {
     requestAnimationFrame(animate);
@@ -18,34 +18,10 @@ var animate = function() {
 
 animate();
 
-function dibujarLineas(caso){
-    var material;
-    var geometry = new THREE.Geometry();
-    switch(caso){
-        case 1:
-            material = new THREE.LineBasicMaterial({ color: 0xDE4343 });
-            geometry.vertices.push(
-            new THREE.Vector3(0, 0, 0 ),
-            new THREE.Vector3( 0, 3, 0 ),
-            );
-            break;
-        case 2:
-            material = new THREE.LineBasicMaterial({ color: 0x2FA63B });
-            geometry.vertices.push(
-            new THREE.Vector3(4, 0, 0 ),
-            new THREE.Vector3( 0, 0, 0 ),
-            );
-            break;
-        case 3:
-            material = new THREE.LineBasicMaterial({ color: 0x0830D7 });
-            geometry.vertices.push(
-            new THREE.Vector3(0, 0, 0 ),
-            new THREE.Vector3( -2, -2, 1),
-            );
-            break;
-    }
-
-    var line = new THREE.Line( geometry, material );
-    scene.add( line );
-
+function eje(){
+    var axis = new THREE.AxisHelper(4);
+    axis.position.x = 1;
+    axis.position.y = 1;
+    axis.position.z = 1;
+    scene.add(axis);
 }
